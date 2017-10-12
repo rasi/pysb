5c532483e972f4070f602b91c7085b3c2ad0ad6e
rasi <rasi1983@gmail.com>
2017-10-12 10:24:25 -0700

    Add total_rate to rules for NFSim

--- pysb/core.py
+++ pysb/core.py
@@ -1193,6 +1193,12 @@ class Rule(Component):
         co-transport anything connected to that Monomer by a path in the same
         compartment. If False (default), connected Monomers will remain where
         they were.
+    total_rate: bool, optional
+        If True, the rate is considered to be macroscopic and is not
+        multiplied by the number of reactant molecules during simulation.
+        If False (default), the rate is multiplied by number of reactant
+        molecules.
+        Keyword is used by Bionetgen only for NFSim.
 
     Attributes
     ----------
@@ -1204,7 +1210,7 @@ class Rule(Component):
 
     def __init__(self, name, rule_expression, rate_forward, rate_reverse=None,
                  delete_molecules=False, move_connected=False,
-                 _export=True):
+                 _export=True, total_rate=False):
         Component.__init__(self, name, _export)
         if not isinstance(rule_expression, RuleExpression):
             raise Exception("rule_expression is not a RuleExpression object")
@@ -1219,6 +1225,7 @@ class Rule(Component):
         self.rate_reverse = rate_reverse
         self.delete_molecules = delete_molecules
         self.move_connected = move_connected
+        self.total_rate = total_rate
         # TODO: ensure all numbered sites are referenced exactly twice within each of reactants and products
 
         # Check synthesis products are concrete
